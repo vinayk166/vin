d3637ecdfa44df68e7a2f5c23470724e0bb5e0a6
vinayk166 <132737409+vinayk166@users.noreply.github.com>
2023-05-06 13:43:20 +0530

    first commit

---
+++ Demo/Test.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class Test {
+ public static void main(String[] args) {
+	int a=20;
+	int b=10;
+	System.out.println(a+b);
+}
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author VINAY
+ *
+ */
+module Git {
+}
